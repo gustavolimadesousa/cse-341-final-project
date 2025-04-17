0fec02b7aa368b8950497665775f84c609e750d5
gustavolimadesousa <gustavolimadesousa@hotmail.com>
2025-04-18 00:02:49 +0100

    update links

--- server.js
+++ server.js
@@ -35,14 +35,19 @@ app.use(cors({
 }));  
 app.use("/", require("./routes"));
 
-passport.use(new GitHubStrategy({
-  clientID: process.env.GITHUB_CLIENT_ID,
-  clientSecret: process.env.GITHUB_CLIENT_SECRET,
-  callbackURL: "http://localhost:3000/auth/github/callback",
-}, 
-function(accessToken, refreshToken, profile, done) {
-  return done(null, profile);
-}));  
+passport.use(
+  new GitHubStrategy(
+    {
+      clientID: process.env.GITHUB_CLIENT_ID,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET,
+      callbackURL:
+        "https://cse-341-final-project-yf51.onrender.com/auth/github/callback",
+    },
+    function (accessToken, refreshToken, profile, done) {
+      return done(null, profile);
+    }
+  )
+);  
 
 passport.serializeUser((user, done) => {
   done(null, user);
